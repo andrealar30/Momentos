a489be3efede89f2c3b3f951dd1cdcc457c6d3c8
andrealar30 <dreaguilar098@gmail.com>
2023-04-21 12:06:35 -0600

    botones que cambian de color

--- conceptos/assets/js/main.js
+++ conceptos/assets/js/main.js
@@ -4,3 +4,13 @@ function changeColorToRed(){
 
     refName.style.color="red";
 }
+function changeColorToBlue(){
+    const refName = document.getElementById("name");
+
+    refName.style.color="blue";
+}
+function changeColorToGreen(){
+    const refName = document.getElementById("name");
+
+    refName.style.color="green";
+}
