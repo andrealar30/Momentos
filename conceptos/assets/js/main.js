b35977b9a6ebf989e80dde3b21e73351af58ade3
andrealar30 <dreaguilar098@gmail.com>
2023-04-21 13:49:10 -0600

    boton de reset

--- conceptos/assets/js/main.js
+++ conceptos/assets/js/main.js
@@ -14,3 +14,15 @@ function changeColorToGreen(){
 
     refName.style.color="green";
 }
+function changeColorParagraph (color, refObj){
+    console.log(refObj);
+    refObj.style.color= color;
+}
+function resetColors(){
+    const refObjs = document.getElementsByClassName("text-color");
+    console.log(refObjs);
+    for (let index = 0; index < refObjs.length; index++) {
+        const element = refObjs[index];
+        element.style.color ="gray";
+    }
+}
