7e78349e4b387c846ebd939c729a95e07a83e6d1
andrealar30 <dreaguilar098@gmail.com>
2023-04-21 12:13:49 -0600

    añadir colores a los botones

--- conceptos/assets/js/main.js
+++ conceptos/assets/js/main.js
@@ -1,5 +1,5 @@
 console.log("Hola desde un script externo");
-function changeColorToRed(){
+function changeColorToRed(color){
     const refName = document.getElementById("name");
 
     refName.style.color="red";
